772b4f07d689f6cbaea4ba21abb81fbe52b49cd7
Sudhakarreddyvari <reddyvarisudhakarreddy@gmail.com>
2025-03-16 12:39:01 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -41,6 +41,10 @@ app.get("/", (req, res) => {
   res.send("LearnX Server is running!")
 })
 
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`))
+
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
